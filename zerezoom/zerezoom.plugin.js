a83f89cc43afa2710374706494ad9945a2538fda
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-04-20 20:29:46 -0500

    Update and rename zerezoom.plugin.js to zerezoom/zerezoom.plugin.js

--- zerezoom/zerezoom.plugin.js
+++ zerezoom/zerezoom.plugin.js
@@ -1,5 +1,13 @@
-//module.exports = class ZereZoom {start() {
-BdApi.showToast('zere_zoom');
+/**
+* @name zerezoom
+* @description zerezoom
+* @author zerezoom
+* @version zerezoom
+* @invite zerezoom
+*/
+
+module.exports = class ZereZoom {start() {
+        BdApi.showToast('zere_zoom');
         BdApi.showConfirmationModal('zere_zoom', 'zere_zoom', {cancelText: 'zere_zoom', confirmText: 'zere_zoom'});
         const zerezoom = 'https://cdn.discordapp.com/emojis/477825238172958730.png?v=1';
         const zerezoom2 = document.querySelectorAll('[class^="userName" i], .title-29uC1r, .topic-TCb_qw, .roleColor-rz2vM0, .name-1jkAdW, .channelName-2YrOjO, .overflow-WK9Ogt, .markup-2BOw-j, .textContent-3N7xXx');
